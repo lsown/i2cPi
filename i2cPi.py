57dfb14850a08285f49bc10e5a18cd956e0f711d
lsown <aureias@gmail.com>
2020-10-22 15:58:24 -0700

    updated fanPWM

--- i2cPi.py
+++ i2cPi.py
@@ -115,7 +115,7 @@ class i2cPi:
             if fan == 1:
                 self.bus.write_byte_data(0x2c, 0x32, dutyCycle_conv) #set PWM for fan 1
                 print('Test register is %s' %self.bus.read_byte(0x2c, 0x00))
-                print('PWM1 Register 0x32 is set to %s%' %self.bus.read_byte(0x2c, 0x00)*0.39)
+                print('PWM1 Register 0x32 is set to %s percent' %self.bus.read_byte(0x2c, 0x00)*0.39)
             elif fan == 2:
                 self.bus.write_byte_data(0x2c, 0x33, dutyCycle_conv) #set PWM for fan 2
             elif fan == 3:
