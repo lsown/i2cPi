bc1d3471ca005d2c9150c29ebe48bebb514b3368
lsown <aureias@gmail.com>
2020-10-30 17:36:12 -0700

    'test'

--- voxaDisplay.py
+++ voxaDisplay.py
@@ -28,7 +28,7 @@ class oledDisplay:
         
         #define font for display
         try:
-            self.font = ImageFont.truetype('arial.ttf', 10)
+            self.font = ImageFont.truetype('arial.ttf', 15)
             #self.font = ImageFont.load_default() #default sizing is 6,11, lets use a nicer font.
 
         except:
