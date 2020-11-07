d84a87e58cf7d3d043d0f15e4c0cb02fce4978d6
lsown <aureias@gmail.com>
2020-11-06 18:25:58 -0800

    added

--- VoxaDisplay.py
+++ VoxaDisplay.py
@@ -176,8 +176,12 @@ class OledButtons:
         try:
             input_register_value = self.bus.read_byte_data(self.address, self.input_register_pointer)
             logging.info('queryButton: Input register read value is %s.' %bin(input_register_value))
-            time.sleep(0.1) #Lets give a small timeout and then re-read register to cleanup and pull ALERT back up in case it failed to go back up. 
+
+            # time.sleep(0.1) #Lets give a small timeout and then re-read register to cleanup and pull ALERT back up in case it failed to go back up. 
             logging.info('queryButton: Clean-up register - just in case ALERT is pulled low. Value read is %s' %bin(self.bus.read_byte_data(0x49, 0x00)))
+            
+            time.sleep(0.5) #lets delay 1/2 second for a button push, then re-read down there
+            input_register_value = self.bus.read_byte_data(self.address, self.input_register_pointer)
             return input_register_value
             #global exit_loop
             #exit_loop = True
