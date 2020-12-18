7616dac8a00b6fbd1d6336d23f66ac1cd66983e5
lsown <aureias@gmail.com>
2020-12-18 14:40:25 -0800

    added updated drawing

--- VoxaDisplay.py
+++ VoxaDisplay.py
@@ -96,10 +96,23 @@ class OledDisplay:
         elif position =='bottom':
             self.drawObj.text((self.oled.width//2 - font_width//2, self.oled.height//2 + 3), text, font=self.font, fill=255)
 
-    def drawCheckText(self, text, xCheck=2, yCheck=2, xText=18, yText=2):
+    def drawCheckText(self, text, column = 1, row = 1, xCheck=2, yCheck=2, xText=18, yText=2):
         '''Draws check box size of 11 + 5 pxl spacers + WORD'''
+        if column === 1:
+            pass
+        else if column === 2:
+            xCheck += 42
+            xText += 42
+        else if column === 3:
+            xCheck += 84
+            xText += 42
+        if row === 1:
+            pass
+        else if row === 2:
+            yCheck += 16
+            yText += 16
         self.drawObj.rectangle([(xCheck,yCheck), (xCheck+11, yCheck+11)], fill=0, outline=1, width = 1)
-        self.drawObj.text([(18, 2)], text=text, fill=1)
+        self.drawObj.text([(xText, yText)], text=text, fill=1)
 
     def drawWifi(self, x=0, y=2, status='ok'):
         '''Draws wifi symbol. If status error, cuts it out and adds exclamation point.'''
